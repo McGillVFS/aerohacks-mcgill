05b960bed3dc7ac2d32161ddd1b06863d3d12ba2
ScaredBow <31490393+ScaredBow@users.noreply.github.com>
2026-02-06 19:05:03 -0500

    Switch register API to service role

--- api/register.js
+++ api/register.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 const supabaseTableUrl = supabaseUrl ? `${supabaseUrl}/rest/v1/registrations` : null;
 
 const allowHeaders = [
